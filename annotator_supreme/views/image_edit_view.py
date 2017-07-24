5f2bf15707cc01e988a5aba044e00912e0c67ee1
Gustavo Führ <gustavofuhr87@gmail.com>
2017-07-24 14:29:08 -0300

    forget view

---
+++ annotator_supreme/views/image_edit_view.py
@@ -0,0 +1,42 @@
+import flask
+from flask.ext.classy import FlaskView, route, request
+from annotator_supreme.models.bbox_model import BBox
+from annotator_supreme.controllers.image_controller import ImageController
+from annotator_supreme.controllers.image_utils import ImageUtils
+from annotator_supreme.views import view_tools
+from annotator_supreme.views import error_views
+import cv2
+
+class ImageEditView(FlaskView):
+    route_base = '/'
+
+    def __init__(self):
+        self.controller = ImageController()
+
+    @route('/image/edit/rotate/<dataset>/<imageid>', methods=['GET'])
+    def rotate_image(self, dataset, imageid):
+        (ok, error, orientation) = view_tools.get_param_from_request(request, "orientation")
+        if not ok or orientation == "":
+            raise error_views.InvalidParametersError("Need to specify the orientation for the rotation: cw or ccw")
+
+        (ok, error) = self.controller.rotate_image(dataset, imageid, orientation)
+        if not ok:
+            raise error_views.InvalidParametersError(error)
+
+        return "", 200
+
+
+    @route('/image/edit/flip/<dataset>/<imageid>', methods=['GET'])
+    def flip_image(self, dataset, imageid):
+        (ok, error, direction) = view_tools.get_param_from_request(request, "direction")
+        if not ok or direction == "":
+            raise error_views.InvalidParametersError("Need to specify the direction for the rotation: h or v")
+
+        (ok, error) = self.controller.flip_image(dataset, imageid, direction)
+        if not ok:
+            raise error_views.InvalidParametersError(error)
+
+        return "", 200
+
+
+        
