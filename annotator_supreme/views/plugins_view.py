9c9a223deaff01a64aebf81859f9696a0ac79e74
Gustavo Führ <gustavofuhr87@gmail.com>
2017-07-26 21:34:43 -0300

    Made a new route to apply a plugin only to a fold

--- annotator_supreme/views/plugins_view.py
+++ annotator_supreme/views/plugins_view.py
@@ -54,6 +54,26 @@ class PluginsView(FlaskView):
         return flask.jsonify({"plugin_response": plugin_res})
 
 
+    @route('/plugins/process/<dataset>/<partition>', methods=['GET'])
+    def get_plugin_process_partition(self, dataset, partition):
+        self.get_plugin_from_request(request)
+
+        (ok, error, partition) = view_tools.get_param_from_request(request, 'partition')
+        if not ok or (partition != "training" and partition != "testing"):
+            raise error_views.InvalidParametersError("Partition not informed or invalid.")
+        
+        self.controller.init_plugin(self.dataset_controller.get_dataset(dataset))
+
+        all_imgs = ImageController.all_images(dataset)
+        for im_obj in all_imgs:
+            if im_obj == DatasetController.partition_id(im_obj["partition"]):
+                (img, anno) = self.get_image_and_anno(dataset, im_obj['phash'])
+                (img, anno) = self.controller.process(img, anno)
+        
+        plugin_res = self.controller.end_plugin()
+        return flask.jsonify({"plugin_response": plugin_res})
+
+
     @route('/plugins/all', methods=['GET'])
     def get_plugin_all(self):
         plugins = self.controller.get_all_plugins()
