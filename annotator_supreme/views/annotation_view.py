437e1453255a5a80aa47d0b2e891aa106709671f
Gustavo Führ <gustavofuhr87@gmail.com>
2017-12-28 20:08:19 -0200

    ADD: route to get all annotations from a dataset, with pagination

--- annotator_supreme/views/annotation_view.py
+++ annotator_supreme/views/annotation_view.py
@@ -4,6 +4,7 @@ from annotator_supreme.models.bbox_model import BBox
 from annotator_supreme.controllers.image_controller import ImageController
 from annotator_supreme.views import view_tools
 from annotator_supreme.views import error_views
+from annotator_supreme import app
 import cv2
 
 class AnnoView(FlaskView):
@@ -18,6 +19,44 @@ class AnnoView(FlaskView):
         image_dict = view_tools.image_to_dict(image)
 
         return flask.jsonify(image_dict)
+
+    def paginate(self, list, page_number, page_size):
+        start_elem = page_size * (page_number - 1)
+
+        return list[start_elem:start_elem+page_size]
+
+    @route('/image/anno/<dataset>/all', methods=['GET'])
+    def get_all_dataset_anno(self, dataset):
+        all_images = ImageController.all_images(dataset)
+    
+        all_annotations = []
+        for img in all_images:
+            app.logger.info("img: "+str(img["annotation"]))
+            for i, anno in enumerate(img["annotation"]):
+                d = {
+                    "top": anno.top,
+                    "bottom": anno.bottom,
+                    "left": anno.left,
+                    "right": anno.right,
+                    "labels": anno.labels,
+                    "ignore": anno.ignore,
+                    "image_url": img["url"],
+                    "anno_ith": i
+                }
+                all_annotations.append(d)
+            
+
+        ok_pagesize, _, page_size = view_tools.get_param_from_request(request, 'itemsPerPage')
+        ok_page, _, page_number = view_tools.get_param_from_request(request, 'pageNumber')
+
+        if ok_pagesize and ok_page:
+            return flask.jsonify({
+                "annotations": self.paginate(all_annotations, int(page_number), int(page_size)),
+                "pageNumber": int(page_number),
+                "itemsPerPage": int(page_size),
+                "totalPages": len(all_annotations)//int(page_size)})
+        else:
+            return flask.jsonify({"annotations": all_annotations})
 
     @route('/image/has_annotation/<dataset>/<imageid>', methods=['GET'])
     def get_image_has_anno(self, dataset, imageid):
