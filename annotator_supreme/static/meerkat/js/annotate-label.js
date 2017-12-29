2c6edfb7e4175025a89de728ac6314f6cc4d4c6b
Gustavo Führ <gustavofuhr87@gmail.com>
2017-12-29 17:30:20 -0200

    UPD: changes to work better on mobile

--- annotator_supreme/static/meerkat/js/annotate-label.js
+++ annotator_supreme/static/meerkat/js/annotate-label.js
@@ -6,6 +6,15 @@
         clear_btn = $("#clear-files-btn");
 
     AnnotateLabel.init = function () {
+        this.isMobile = true; //initiate as false
+        // device detection
+        if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+            || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) this.isMobile = true;
+
+        if (this.isMobile) {
+            $("nav").remove();
+        }
+
         this.populateImages();
         this.bindSaveButton();
 
@@ -90,7 +99,13 @@
 
         this.dataset = getParameterByName("dataset");
         this.pageNumber = getParameterByName("pageNumber") || 1;
-        this.itemsPerPage = 16; //TODO: in mobile could be one
+        if (this.isMobile) {
+            this.itemsPerPage = 1;     
+        }
+        else {
+            this.itemsPerPage = 16;    
+        }
+        
 
         var self = this;
         $.ajax({
@@ -101,7 +116,9 @@
                 // window.location.reload();
                 self.pageCount = data.totalPages;
                 self.pageNumber = data.pageNumber;
+                
                 self.addPaginationControllers();
+
                 self.updatePreviousNextPaginationControllers();
                 self.bindPagination();
 
@@ -137,24 +154,26 @@
             $('.paginate_button.people-separator').remove();
 
             if (this.pageCount > 1) {
-                if (this.pageNumber < 5) {
-                    first = 1;
-                    last = Math.min(5, this.pageCount);
-                } else if (this.pageCount - this.pageNumber < 5) {
-                    first = Math.max(1, this.pageCount - 5);
-                    last = this.pageCount;
-                }
+                if (!this.isMobile) {
+                    if (this.pageNumber < 5) {
+                        first = 1;
+                        last = Math.min(5, this.pageCount);
+                    } else if (this.pageCount - this.pageNumber < 5) {
+                        first = Math.max(1, this.pageCount - 5);
+                        last = this.pageCount;
+                    }
 
-                for (pageNumber = last; pageNumber >= first; pageNumber--) {
-                    page = this.createPageButton(pageNumber);
-                    $('#people-database-prev').after(page);
-                }
+                    for (pageNumber = last; pageNumber >= first; pageNumber--) {
+                        page = this.createPageButton(pageNumber);
+                        $('#people-database-prev').after(page);
+                    }
 
-                if (first != 1) {
-                    this.addPageButtons(1, true);
-                }
-                if (last != this.pageCount) {
-                    this.addPageButtons(this.pageCount, false);
+                    if (first != 1) {
+                        this.addPageButtons(1, true);
+                    }
+                    if (last != this.pageCount) {
+                        this.addPageButtons(this.pageCount, false);
+                    }
                 }
                 $('#people-database-next').show();
                 $('#people-database-prev').show();
